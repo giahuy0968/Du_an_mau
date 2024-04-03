721fa35cfeb78f888feb474c35df4f241585530b
Nguyen Gia Huy <huyng9123@ut.edu.vn>
2024-04-03 14:37:38 +0700

    Thay doi dong 8

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ttkbootstrap.validation import add_regex_validation
 import random
 from PIL import Image, ImageTk
 
-
+Hello 
 class NutritionManager(ttk.Frame):
     def __init__(self, master_window):
         super().__init__(master_window, padding=(20, 10))
