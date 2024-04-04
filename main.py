bead9b497e51b3b6e42911d0ad4808cf74019831
TuanN05 <tuannt1436@ut.edu.vn>
2024-04-04 14:52:46 +0700

    Them thai tuan

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import random
 from PIL import Image, ImageTk
 
 print ("Them xin chao !")
+print("Thai Dui")
 class NutritionManager(ttk.Frame):
     def __init__(self, master_window):
         super().__init__(master_window, padding=(20, 10))
